7d085513e1b59d847c67f43f593406d186bb7a1e
Tom <42585270+tjhansult@users.noreply.github.com>
2022-12-03 15:10:02 +0100

    Controller Stub implementation

--- src/controller/GameLogic.java
+++ src/controller/GameLogic.java
@@ -1,4 +1,31 @@
 package controller;
 
+import model.Card;
+import model.Deck;
+import model.Game;
+
+import java.util.ArrayList;
+
 public class GameLogic {
+
+    public Game startGame(Game game) {
+        return game;
+    }
+    public void runGame() {}
+
+    public Game endGame(Game game) {
+        return game;
+    }
+    public Deck shuffleDeck (Deck deck){
+        return deck;
+    }
+    public void distributeHands() {}
+    public void nextTurn() {}
+    public boolean validMove() {
+        return false;
+    }
+    public void evaluateNewCard() {}
+
+
+
 }
--- src/controller/PlayerLogic.java
+++ src/controller/PlayerLogic.java
@@ -1,4 +1,14 @@
 package controller;
 
+import model.Card;
+
 public class PlayerLogic {
+    public void playCard() {}
+    public void drawCard() {}
+    public Card.Color pickColor() {
+        return Card.Color.BLUE;
+    }
+    public void sayUno() {}
+    public void stopUno() {}
+    public void win() {}
 }
